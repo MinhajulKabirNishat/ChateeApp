9552a097c45f4be256ba4d410604303d1c6e4351
Minhajul Kabir Nishat <kabirminhajul7@gmail.com>
2026-01-08 01:30:10 +0600

    add message logging with timestamps on server

--- chat.sh
+++ chat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 PORT=5000
+LOG_FILE="logs/chat.log"
+
 
 
 echo "Enter your username:"
@@ -10,13 +12,19 @@ read USERNAME
 if [ "$1" == "server" ]; then
     echo "🟢 Chat server started on port $PORT"
     echo "Waiting for client..."
-    ncat -l -p $PORT
+    echo "Messages will be logged in $LOG_FILE"
+
+    ncat -l -p $PORT | while read MESSAGE; do
+        TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+        echo "[$TIMESTAMP] $MESSAGE" | tee -a "$LOG_FILE"
+    done
+
 
 
 elif [ "$1" == "client" ]; then
     if [ -z "$2" ]; then
         echo "❌ Usage: ./chat.sh client <SERVER_IP>"
-        exit 1
+        exit 1j
     fi
 
     echo "🔵 Connected to server at $2:$PORT"
